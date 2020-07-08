f5cc0716422a6cf469b6914da7012f371a4c4931
JohnneSouza <jhonnesf5@gmail.com>
2020-07-07 23:18:35 -0300

    Derivative of 1 over x

--- src/pages/one_over_x/one_over_x.py
+++ src/pages/one_over_x/one_over_x.py
@@ -9,21 +9,33 @@ def select():
     if opt == 'Função':
         x_var = st.number_input('Insira o valor de 𝑥')
         st.title("Se 𝑥 = {}".format(x_var))
+        if x_var == 0:
+            st.markdown(
+                """
+                Não é possível realizar divisão por 0 (zero) 
+                veja os detalhes neste [video](https://www.youtube.com/watch?v=J2z5uzqxJNU) sobre divisão por 0.
+                """)
         try:
             st.title("𝑓 (𝑥) = 1/{}".format(x_var))
             st.title("𝑓 ({}) = {:.4f}".format(x_var, 1/x_var))
         except:
-            if x_var == 0:
-                st.markdown(
-                    """
-                    Não é possível realizar divisão por 0 (zero) 
-                    veja os detalhes neste [video](https://www.youtube.com/watch?v=J2z5uzqxJNU) sobre divisão por 0.
-                    """)
             st.title("Tente trocar o valor de X")
 
     if opt == 'Derivada':
-        st.title("Derivada")
-        st.title("Nao implementado")
+        x_var = st.number_input('Insira o valor de 𝑥')
+        st.title("Se 𝑥 = {}".format(x_var))
+        if x_var == 0:
+            st.markdown(
+                """
+                Não é possível realizar divisão por 0 (zero) 
+                veja os detalhes neste [video](https://www.youtube.com/watch?v=J2z5uzqxJNU) sobre divisão por 0.
+                """)
+        try:
+            st.title("𝑓' (𝑥) = 1/{}".format(x_var))
+            st.title("𝑓' ({}) = 1/{}^2".format(x_var, 1 / x_var))
+            st.title("𝑓' ({}) = {:.4f}".format(x_var, 1 / x_var**2))
+        except:
+            st.title("Tente trocar o valor de X")
 
     if opt == 'Integral Definida':
         st.title("Integral Definida")
